cd1a3113f1f45f77f98159230c7ba004c87791a7
Pavlo Chypiga <chipiga@users.noreply.github.com>
2025-08-21 13:35:34 +0200

    Fix bug with login termination after page reload

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -7,6 +7,7 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signOut,
+  onAuthStateChanged
 } from "firebase/auth";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 
